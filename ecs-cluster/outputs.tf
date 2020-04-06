0937eda7e1ddf5a9640cf141201e12c3a40f4dd3
Dusan Stojnic <dusanstojnic@Dusans-MBP.home>
2020-04-05 22:04:01 -0400

    working permissions for grafana cloudwatch datasource

--- ecs-cluster/outputs.tf
+++ ecs-cluster/outputs.tf
@@ -5,3 +5,7 @@ output vpc_id {
 output grafana_aws_iam_role {
   value = module.grafana-deploy.aws_iam_role
 }
+
+output grafana_dns_address {
+  output = format("Grafana URL: %s", var.route_53_zone)
+}
